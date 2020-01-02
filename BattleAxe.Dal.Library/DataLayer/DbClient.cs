0c4da7ee511d5aa29664673e9503c54905b10814
Warwick858 <lo40ops@gmail.com>
2020-01-01 19:10:59 -0700

    Add project files.

---
+++ BattleAxe.Dal.Library/DataLayer/DbClient.cs
@@ -0,0 +1,147 @@
+﻿// ******************************************************************************************************************
+//  This file is part of BattleAxe.Dal.
+//
+//  BattleAxe.Dal - web service for the data access layer.
+//  Copyright(C)  2020  James LoForti
+//  Contact Info: jamesloforti@gmail.com
+//
+//  BattleAxe.Dal is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.If not, see<https://www.gnu.org/licenses/>.
+//									     ____.           .____             _____  _______   
+//									    |    |           |    |    ____   /  |  | \   _  \  
+//									    |    |   ______  |    |   /  _ \ /   |  |_/  /_\  \ 
+//									/\__|    |  /_____/  |    |__(  <_> )    ^   /\  \_/   \
+//									\________|           |_______ \____/\____   |  \_____  /
+//									                             \/          |__|        \/ 
+//
+// ******************************************************************************************************************
+//
+using BattleAxe.Dal.Common.Model;
+using Dapper;
+using Serilog;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text.Json;
+
+namespace BattleAxe.Dal.Library.DataLayer
+{
+	/// <summary>
+	/// CRUD methods for database.
+	/// </summary>
+	public class DbClient
+	{
+		private readonly DbConnectionCreator _dbConnectionCreator;
+
+		public DbClient(DbConnectionCreator dbConnectionCreator)
+		{
+			_dbConnectionCreator = dbConnectionCreator;
+		}
+
+		/// <summary>
+		/// CREATE
+		/// </summary>
+		public int CreateRecord(GenericRequest record)
+		{
+			Log.Information($"Attempting to create record: {JsonSerializer.Serialize(record)} in the database.");
+
+			using var dbConnection = _dbConnectionCreator.CreateConnection();
+
+			var parms = new DynamicParameters();
+			parms.Add("Message", record.Message);
+			parms.Add("Id", record.Id);
+
+			//Return parm
+			parms.Add("Id", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+
+			var id = dbConnection.ExecuteScalar<int>("stored_proc_name", parms, commandType: CommandType.StoredProcedure);
+
+			Log.Information($"Created record {record} and received id {id} from the database.");
+
+			return id;
+		} // end method
+
+		/// <summary>
+		/// READ SINGLE RECORD
+		/// </summary>
+		public string GetRecord(int id)
+		{
+			Log.Information($"Attempting to get record using id {id} from the database.");
+
+			using var dbConnection = _dbConnectionCreator.CreateConnection();
+
+			var record = dbConnection.QuerySingle<string>("stored_proc_name", id, commandType: CommandType.StoredProcedure);
+
+			Log.Information($"Received record {record} using id {id} from the database.");
+
+			return record;
+		} // end method
+
+		/// <summary>
+		/// READ SINGLE RECORD
+		/// </summary>
+		public List<GenericRequest> GetRecords()
+		{
+			Log.Information($"Attempting to get all records from the database.");
+
+			using var dbConnection = _dbConnectionCreator.CreateConnection();
+
+			var records = dbConnection.Query<GenericRequest>("stored_proc_name", commandType: CommandType.StoredProcedure).ToList();
+
+			Log.Information($"Received records {JsonSerializer.Serialize(records)} from the database.");
+
+			return records;
+		} // end method
+
+		/// <summary>
+		/// UPDATE
+		/// </summary>
+		public bool UpdateRecord(GenericRequest record)
+		{
+			Log.Information($"Attempting to update record: {JsonSerializer.Serialize(record)} in the database.");
+
+			using var dbConnection = _dbConnectionCreator.CreateConnection();
+
+			var parms = new DynamicParameters();
+			parms.Add("Message", record.Message);
+			parms.Add("Id", record.Id);
+
+			//Return parm
+			parms.Add("Id", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+
+			var result = dbConnection.Execute("stored_proc_name", parms, commandType: CommandType.StoredProcedure);
+			var outcome = (result == -1);
+
+			Log.Information($"Updated record {record} and received result {outcome} from the database.");
+
+			return outcome;
+		} // end method
+
+		/// <summary>
+		/// DELETE RECORD
+		/// </summary>
+		public void DeleteRecord(int id)
+		{
+			Log.Information($"Attempting to delete record using id {id} from the database.");
+
+			using var dbConnection = _dbConnectionCreator.CreateConnection();
+
+			var parms = new DynamicParameters();
+			parms.Add("Id", id);
+
+			var record = dbConnection.ExecuteScalar("stored_proc_name", parms, commandType: CommandType.StoredProcedure);
+
+			Log.Information($"Deleted record using id {id} from the database.");
+		} // end method
+	} // end class
+} // end namespace
---
+++ README.md
@@ -0,0 +1,13 @@
+![JRL Logo](http://jimmyloforti.com/_common/images/jrl_logo2.png)
+
+## Application Name: BattleAxe.Dal ##
+
+* Solution Types: .NET Web Core
+
+* Description: web service for the data access layer.
+
+* Nuget Packages:
+	* Dapper - 2.0.30
+	* Serilog.AspNetCore - 3.1.0
+	* Swashbuckle.AspNetCore - 5.0.0-rc4
+	* Swashbuckle.AspNetCore.Annotations - 5.0.0-rc4
